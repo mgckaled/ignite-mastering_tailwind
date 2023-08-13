257b95b223c3db987a8b8d816519ebb7d102285a
mgckaled <marcelgckaled@gmail.com>
2023-08-13 17:07:30 -0300

    chore: config eslint and prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,16 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/**/*.tsx',
-    './src/**/**/*.tsx',
-    './src/**/**/**/*.tsx',
-  ],
-   theme: {
+  content: ['./src/**/*.tsx', './src/**/**/*.tsx', './src/**/**/**/*.tsx'],
+  theme: {
     extend: {
       maxWidth: {
-        app: '700px'
+        app: '700px',
       },
 
       colors: {
-        marcel: '#333'
-      }
+        marcel: '#333',
+      },
     },
   },
   plugins: [],
